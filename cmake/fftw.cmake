fd9069c60f16c826ee4e29c57a69059604d4d07a
Franz R. Sattler <fsattler@physik.uni-bielefeld.de>
2026-04-09 20:10:52 +0200

    Added option to fix FFTW dir

--- cmake/fftw.cmake
+++ cmake/fftw.cmake
@@ -1,26 +1,26 @@
-find_path(FFTW3_INCLUDES fftw3.h HINTS ${FFTW_DIR}/include)
+find_path(FFTW3_INCLUDES fftw3.h HINTS ${PARAFAFT_FFTW_DIR}/include)
 mark_as_advanced(FFTW3_INCLUDES)
 
 # Find the FFTW libraries
-find_library(FFTW3_LIB fftw3 HINTS ${FFTW_DIR}/lib)
+find_library(FFTW3_LIB fftw3 HINTS ${PARAFAFT_FFTW_DIR}/lib)
 mark_as_advanced(FFTW3_LIB)
 
 # Find optional threaded FFTW libraries Priority: libfftw3_threads (POSIX
 # threads) > fftw3_omp (OpenMP) > serial
-find_library(FFTW3_THREADS_LIB fftw3_threads HINTS ${FFTW_DIR}/lib)
+find_library(FFTW3_THREADS_LIB fftw3_threads HINTS ${PARAFAFT_FFTW_DIR}/lib)
 mark_as_advanced(FFTW3_THREADS_LIB)
 
-find_library(FFTW3_OMP_LIB fftw3_omp HINTS ${FFTW_DIR}/lib)
+find_library(FFTW3_OMP_LIB fftw3_omp HINTS ${PARAFAFT_FFTW_DIR}/lib)
 mark_as_advanced(FFTW3_OMP_LIB)
 
 # If float support is enabled, find the single-precision FFTW libraries.
 if(Float)
-  find_library(FFTW3F_LIB fftw3f HINTS ${FFTW_DIR}/lib)
+  find_library(FFTW3F_LIB fftw3f HINTS ${PARAFAFT_FFTW_DIR}/lib)
   mark_as_advanced(FFTW3F_LIB)
   # Also find threaded versions if available
-  find_library(FFTW3F_THREADS_LIB fftw3f_threads HINTS ${FFTW_DIR}/lib)
+  find_library(FFTW3F_THREADS_LIB fftw3f_threads HINTS ${PARAFAFT_FFTW_DIR}/lib)
   mark_as_advanced(FFTW3F_THREADS_LIB)
-  find_library(FFTW3F_OMP_LIB fftw3f_omp HINTS ${FFTW_DIR}/lib)
+  find_library(FFTW3F_OMP_LIB fftw3f_omp HINTS ${PARAFAFT_FFTW_DIR}/lib)
   mark_as_advanced(FFTW3F_OMP_LIB)
 endif()
 
