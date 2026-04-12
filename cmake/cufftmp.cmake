dc671f02bb0bd5ba6df04a13fcce1bb50bb12cf5
Franz R. Sattler <fsattler@physik.uni-bielefeld.de>
2026-04-12 20:23:16 +0200

    .

--- cmake/cufftmp.cmake
+++ cmake/cufftmp.cmake
@@ -20,91 +20,90 @@
 #   nvshmem::host        - NVSHMEM host library
 
 if(CMAKE_CXX_COMPILER_ID MATCHES "NVHPC")
-  add_compile_options(-cudalib=cufftmp)
-endif()
+else()
+  set(CUFFTMP_HOME "$ENV{CUFFTMP_HOME}" CACHE PATH "Path to cuFFTMp installation")
+  set(NVSHMEM_HOME "$ENV{NVSHMEM_HOME}" CACHE PATH "Path to NVSHMEM installation")
 
-set(CUFFTMP_HOME "$ENV{CUFFTMP_HOME}" CACHE PATH "Path to cuFFTMp installation")
-set(NVSHMEM_HOME "$ENV{NVSHMEM_HOME}" CACHE PATH "Path to NVSHMEM installation")
+  # Build search paths from HPC SDK if not explicitly set
+  set(_cufftmp_search_paths)
+  set(_nvshmem_search_paths)
 
-# Build search paths from HPC SDK if not explicitly set
-set(_cufftmp_search_paths)
-set(_nvshmem_search_paths)
-
-if(CUFFTMP_HOME)
-  list(APPEND _cufftmp_search_paths "${CUFFTMP_HOME}")
-endif()
-if(NVSHMEM_HOME)
-  list(APPEND _nvshmem_search_paths "${NVSHMEM_HOME}")
-endif()
-
-# Auto-detect HPC SDK paths
-foreach(_base /opt/nvidia/hpc_sdk/Linux_x86_64 /opt/nvidia/hpc_sdk/Linux_aarch64)
-  if(IS_DIRECTORY "${_base}")
-    file(GLOB _versions "${_base}/*")
-    foreach(_ver ${_versions})
-      if(IS_DIRECTORY "${_ver}/math_libs")
-        list(APPEND _cufftmp_search_paths "${_ver}/math_libs")
-      endif()
-      if(IS_DIRECTORY "${_ver}/comm_libs/nvshmem")
-        list(APPEND _nvshmem_search_paths "${_ver}/comm_libs/nvshmem")
-      endif()
-    endforeach()
+  if(CUFFTMP_HOME)
+    list(APPEND _cufftmp_search_paths "${CUFFTMP_HOME}")
   endif()
-endforeach()
+  if(NVSHMEM_HOME)
+    list(APPEND _nvshmem_search_paths "${NVSHMEM_HOME}")
+  endif()
 
-# Find cuFFTMp header
-find_path(CUFFTMP_INCLUDE_DIR
+  # Auto-detect HPC SDK paths
+  foreach(_base /opt/nvidia/hpc_sdk/Linux_x86_64 /opt/nvidia/hpc_sdk/Linux_aarch64)
+    if(IS_DIRECTORY "${_base}")
+      file(GLOB _versions "${_base}/*")
+      foreach(_ver ${_versions})
+        if(IS_DIRECTORY "${_ver}/math_libs")
+          list(APPEND _cufftmp_search_paths "${_ver}/math_libs")
+        endif()
+        if(IS_DIRECTORY "${_ver}/comm_libs/nvshmem")
+          list(APPEND _nvshmem_search_paths "${_ver}/comm_libs/nvshmem")
+        endif()
+      endforeach()
+    endif()
+  endforeach()
+
+  # Find cuFFTMp header
+  find_path(CUFFTMP_INCLUDE_DIR
   NAMES cufftMp.h
   HINTS ${_cufftmp_search_paths}
   PATH_SUFFIXES include include/cufftmp
 )
 
-# Find cuFFTMp library
-find_library(CUFFTMP_LIBRARY
+  # Find cuFFTMp library
+  find_library(CUFFTMP_LIBRARY
   NAMES cufftMp
   HINTS ${_cufftmp_search_paths}
   PATH_SUFFIXES lib64 lib
 )
 
-# Find NVSHMEM header
-find_path(NVSHMEM_INCLUDE_DIR
+  # Find NVSHMEM header
+  find_path(NVSHMEM_INCLUDE_DIR
   NAMES nvshmem.h
   HINTS ${_nvshmem_search_paths}
   PATH_SUFFIXES include
 )
 
-# Find NVSHMEM host library
-find_library(NVSHMEM_LIBRARY
+  # Find NVSHMEM host library
+  find_library(NVSHMEM_LIBRARY
   NAMES nvshmem_host
   HINTS ${_nvshmem_search_paths}
   PATH_SUFFIXES lib
 )
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(CUFFTMP
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(CUFFTMP
   REQUIRED_VARS CUFFTMP_LIBRARY CUFFTMP_INCLUDE_DIR NVSHMEM_LIBRARY NVSHMEM_INCLUDE_DIR
   FAIL_MESSAGE "cuFFTMp not found. Set CUFFTMP_HOME and NVSHMEM_HOME, or install via NVIDIA HPC SDK."
 )
 
-if(CUFFTMP_FOUND)
-  message(STATUS "Found cuFFTMp: ${CUFFTMP_LIBRARY}")
+  if(CUFFTMP_FOUND)
+    message(STATUS "Found cuFFTMp: ${CUFFTMP_LIBRARY}")
 
-  if(NOT TARGET cufftMp::cufftMp)
-    add_library(cufftMp::cufftMp SHARED IMPORTED)
-    set_target_properties(cufftMp::cufftMp PROPERTIES
+    if(NOT TARGET cufftMp::cufftMp)
+      add_library(cufftMp::cufftMp SHARED IMPORTED)
+      set_target_properties(cufftMp::cufftMp PROPERTIES
       IMPORTED_LOCATION "${CUFFTMP_LIBRARY}"
       INTERFACE_INCLUDE_DIRECTORIES "${CUFFTMP_INCLUDE_DIR}"
     )
-  endif()
+    endif()
 
-  if(NOT TARGET nvshmem::host)
-    add_library(nvshmem::host SHARED IMPORTED)
-    set_target_properties(nvshmem::host PROPERTIES
+    if(NOT TARGET nvshmem::host)
+      add_library(nvshmem::host SHARED IMPORTED)
+      set_target_properties(nvshmem::host PROPERTIES
       IMPORTED_LOCATION "${NVSHMEM_LIBRARY}"
       INTERFACE_INCLUDE_DIRECTORIES "${NVSHMEM_INCLUDE_DIR}"
     )
+    endif()
   endif()
-endif()
 
-mark_as_advanced(CUFFTMP_INCLUDE_DIR CUFFTMP_LIBRARY NVSHMEM_INCLUDE_DIR NVSHMEM_LIBRARY)
+  mark_as_advanced(CUFFTMP_INCLUDE_DIR CUFFTMP_LIBRARY NVSHMEM_INCLUDE_DIR NVSHMEM_LIBRARY)
+
 endif()
